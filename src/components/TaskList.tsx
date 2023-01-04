3b63e1a0415e5d14cc75a2c4836c7c00e776e3ed
Mateus Castellar <mateuscastellar@outlook.com>
2023-01-04 16:17:57 -0300

    feat: Listagem de tarefas cadastradas

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { ITask } from "../interfaces/Task";
+import styles from "./TaskList.module.css";
 
-type Props = {};
+type Props = {
+  taskList: ITask[];
+};
 
-const TaskList = (props: Props) => {
-  return <div>Lista de tarefas</div>;
+const TaskList = ({ taskList }: Props) => {
+  return (
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id} className={styles.task}>
+            <div className={styles.details}>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+
+            <div className={styles.actions}>
+              <i className="bi bi-pencil"></i>
+              <i className="bi bi-trash"></i>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>Não há tarefas cadastradas</p>
+      )}
+    </>
+  );
 };
 
 export default TaskList;
